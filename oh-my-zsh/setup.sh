70171181a211913b821ccd128e6ee1c8bfd430ac
longhongc <longhongc@gmail.com>
2020-07-19 20:32:03 +0800

    Update setup.sh

--- oh-my-zsh/setup.sh
+++ oh-my-zsh/setup.sh
@@ -1,2 +1,18 @@
+#Install oh-my-zsh
 sudo apt install zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
+#Switch Default Shell
+chsh -s $(which zsh)
+
+#Download Plugins
+cd /home/shellhub/.oh-my-zsh/custom/plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions
+
+#Add Plugins in .zshrc
+#plugins=(
+#  git
+#  zsh-syntax-highlighting
+#  zsh-autosuggestions
+#)
